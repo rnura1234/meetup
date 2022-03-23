48c2a06b9007abe264a2dfe83a04fd43ff1f8167
sanjeevs7works <sanjeev@s7works.io>
2022-03-23 16:49:24 +0530

    some change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    reactRoot: true,
-    runtime: 'nodejs' // but it also seems to crash when 'edge' is chosen
-  }
-}
+  // experimental: {
+  //   reactRoot: true,
+  //   runtime: 'nodejs' // but it also seems to crash when 'edge' is chosen
+  // }
+};
